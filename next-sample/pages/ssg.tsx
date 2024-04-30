517aa0e92838bfd2d6700869285e5f1be48930f3
YasudaKaito <ykit.engineer@gmail.com>
2024-04-30 15:19:11 +0900

    3.8.3 getStaticProps

--- next-sample/pages/ssg.tsx
+++ next-sample/pages/ssg.tsx
@@ -1,9 +1,13 @@
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 import Head from 'next/head'
 
-type SSGProps = {}
+type SSGProps = {
+  message: string
+}
 
-const SSG: NextPage<SSGProps> = () => {
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props
+
   return (
     <div>
       {/* Headで囲むと、<head>に配置される */}
@@ -13,9 +17,23 @@ const SSG: NextPage<SSGProps> = () => {
       </Head>
       <main>
         <p>このページは静的サイト生成でビルド時に生成されたページ</p>
+        <p>{message}</p>
       </main>
     </div>
   )
+}
+
+// ビルド時に実行される
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+  const timestamp = new Date().toLocaleString()
+  const message = `${timestamp}にgetStaticPropsが実行されました`
+  console.log(message)
+  // ここで返したpropsをもとにページコンポーネントを描画する
+  return {
+    props: {
+      message
+    }
+  }
 }
 
 export default SSG
