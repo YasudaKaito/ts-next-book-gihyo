4a90eb6952cadcecf62edf8ef4a9b1c57f0a80bb
YasudaKaito <ykit.engineer@gmail.com>
2024-05-02 08:06:49 +0900

    4.3.2 Storybook action

--- next-sample/stories/StyledButton.stories.tsx
+++ next-sample/stories/StyledButton.stories.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react'
 import { Meta, StoryFn } from '@storybook/react'
 import { StyledButton, StyledButtonProps } from '../components/StyledButton'
+import { action } from '@storybook/addon-actions'
 
 // storyの設定
 export default {
   title: 'StyledButton',
   component: StyledButton
+  // argTypes: {
+  //   onClick: { action: 'clicked' }
+  // }
 } as Meta<typeof StyledButton>
 
+const incrementAction = action('increment')
+
 export const Primary: StoryFn<StyledButtonProps> = (props) => {
+  const [count, setCount] = useState(0)
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count)
+    setCount((c) => c + 1)
+  }
+
   return (
-    <StyledButton {...props} variant="primary">
-      Primary
+    <StyledButton {...props} variant="primary" onClick={onClick}>
+      Count: {count}
     </StyledButton>
   )
 }
