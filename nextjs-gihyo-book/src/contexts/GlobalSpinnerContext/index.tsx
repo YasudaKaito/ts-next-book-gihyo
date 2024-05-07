33509b768a93cf1e84154f7606e435fcd9be1aa9
YasudaKaito <ykit.engineer@gmail.com>
2024-05-07 18:55:12 +0900

    6.7.2 organisms/GlobalSpinnerとstory追加

---
+++ nextjs-gihyo-book/src/contexts/GlobalSpinnerContext/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useContext, createContext } from 'react'
+
+const GlobalSpinnerContext = createContext(false)
+const GlobalSpinnerActionsContext = createContext<
+  React.Dispatch<React.SetStateAction<boolean>>
+>(() => {})
+
+// スピナーの表示・非表示を返す関数
+export const useGlobalSpinnerContext = () => useContext(GlobalSpinnerContext)
+
+// スピナーの表示・非表示を変更する関数
+export const useGlobalSpinnerActionsContext = () =>
+  useContext<React.Dispatch<React.SetStateAction<boolean>>>(
+    GlobalSpinnerActionsContext
+  )
+
+interface GlobalSpinnerProviderProps {
+  children?: React.ReactNode
+}
+
+const GlobalSpinnerContextProvider = ({
+  children
+}: GlobalSpinnerProviderProps) => {
+  const [isGlobalSpinnerOn, setGlobalSpinner] = useState(false)
+
+  return (
+    <GlobalSpinnerContext.Provider value={isGlobalSpinnerOn}>
+      <GlobalSpinnerActionsContext.Provider value={setGlobalSpinner}>
+        {children}
+      </GlobalSpinnerActionsContext.Provider>
+    </GlobalSpinnerContext.Provider>
+  )
+}
+
+export default GlobalSpinnerContextProvider
