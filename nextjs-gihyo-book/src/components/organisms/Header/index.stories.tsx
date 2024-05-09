cf654b09554a0d24874d5ffba8d3e77875b2d077
YasudaKaito <ykit.engineer@gmail.com>
2024-05-09 09:05:29 +0900

    6.7.3 organisms/Headerのstory追加

---
+++ nextjs-gihyo-book/src/components/organisms/Header/index.stories.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import { Meta } from '@storybook/react'
+import Header from './index'
+import {
+  ShoppingCartContextProvider,
+  useShoppingCartContext
+} from 'contexts/ShoppingCartContext'
+import { AuthContextProvider } from 'contexts/AuthContext'
+import type { User } from 'types'
+
+export default { title: 'organisms/Header' } as Meta<typeof Header>
+
+export const NoLogin = () => <Header />
+
+export const Login = () => {
+  const authUser: User = {
+    id: 1,
+    username: 'dummy',
+    displayName: 'Taketo Yoshida',
+    email: 'test@example.com',
+    profileImageUrl: '/images/sample/1.jpg',
+    description: ''
+  }
+
+  const ChildComponent = () => {
+    const { addProductToCart } = useShoppingCartContext()
+    // ダミー商品追加
+    useEffect(() => {
+      addProductToCart({
+        id: 1,
+        category: 'book',
+        title: 'Product',
+        description: '',
+        imageUrl: '/images/sample/1.jpg',
+        blurDataUrl: '',
+        price: 1000,
+        condition: 'used',
+        owner: authUser
+      })
+    }, [])
+
+    return <Header />
+  }
+
+  return (
+    <ShoppingCartContextProvider>
+      <AuthContextProvider
+        context={{ apiRootUrl: 'https://dummy' }}
+        authUser={authUser}
+      >
+        <ChildComponent />
+      </AuthContextProvider>
+    </ShoppingCartContextProvider>
+  )
+}
